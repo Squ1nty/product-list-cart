e28b4381d25c213e6abef9a4d5b888853f5b3313
Vincent Dao <vincentdao288@gmail.com>
2025-11-30 22:02:11 +1100

    Initial commit

---
+++ src/jsx/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+  return(
+      <div className="text-base text-center">
+        Challenge by <a className='text-[var(--hyperlink-blue)]' href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+        Coded by <a className='text-[var(--hyperlink-blue)]' href="https://www.frontendmentor.io/profile/Squ1nty">Vincent Dao</a>.
+      </div>
+  );
+}
+
+export default Footer
